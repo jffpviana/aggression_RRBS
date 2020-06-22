be1a5cce3cca95d3acd7cd7a2583b6a1cddfa636
zxh755 <zxh755@student.bham.ac.uk>
2020-06-22 12:46:42 +0100

    fixed mistake (-gzip instead of --gzip

--- scripts/3.trim_galore_raw_data.sh
+++ scripts/3.trim_galore_raw_data.sh
@@ -9,7 +9,7 @@ set -e
 module purge; module load bluebear
 
 module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4 #loading trim galore
-trim_galore -q 20  --phred33 --rrbs –gzip -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/trim_galore_output /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/*.fq
+trim_galore -q 20  --phred33 --rrbs --gzip -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/trim_galore_output /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/*.fq
 
 # -q 20 Trim low-quality ends from reads in addition to adapter removal
 # --phred33 for quality trimming
