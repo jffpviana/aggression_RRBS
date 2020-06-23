5050b5e0401e833cefbf307a785bab2280a900cb
Joana Viana <j.f.viana@exeter.ac.uk>
2020-06-23 12:07:04 +0100

    download and prep genome

---
+++ scripts/0.download_genome.sh
@@ -0,0 +1,11 @@
+module load slurm-interactive #load module
+fisbatch_screen --ntasks 8 --time 1:0:0
+
+#mkdir  /rds/projects/v/vianaj-genomics-brain-development/MATRICS/genome # create folder
+
+cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/genome # change directory
+
+
+wget ftp://ftp.ensembl.org/pub/release-100/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+
+gzip -d Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
---
+++ scripts/0.prepare_genome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks 8
+#SBATCH --time 1-0:0:0
+#SBATCH --qos bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+nce genome
+module load Bismark/0.22.3-foss-2019b #load bismark
+
+bismark_genome_preparation --bowtie2 --verbose /rds/projects/v/vianaj-genomics-brain-development/MATRICS/genome
