e427e0f3b3da3efc910f3ebbe890af25fcdaa0ba
zxh755 <zxh755@student.bham.ac.uk>
2020-06-23 15:08:14 +0100

    trim galore fastqc

---
+++ scripts/5.fastqc_trim_galore_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --ntasks 5
+#SBATCH --time 50:00:00
+#SBATCH --qos bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear 
+module load FastQC/0.11.9-Java-11 #load the module fastqc
+
+cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/trim_galore_output #change directory
+fastqc *.fq -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/raw_data/trim_galore_output/trim_galore_fastqc # run fastqc for all samples and output in the directory of the respective cohort
+
