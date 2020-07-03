09fb3c4996f4397020525034022ed666378e4e80
zxh755 <zxh755@student.bham.ac.uk>
2020-07-03 15:55:16 +0100

    added column names to list of dtata frames

--- R_scripts/spikeins_script.R
+++ R_scripts/spikeins_script.R
@@ -1,21 +1,20 @@
-# made methylated data into a list
-
+# change directory
 setwd("/Volumes/rdsprojects/v/vianaj-genomics-brain-development/MATRICS/
       bismark_methylation_extractor/spikeins/methylated/")
+
+# added files to list of data frames
 methylated_data <- lapply(Sys.glob("*.bismark.cov"), read.table)
+
+# same for unmethylated data
 setwd("/Volumes/rdsprojects/v/vianaj-genomics-brain-development/MATRICS/
       bismark_methylation_extractor/spikeins/unmethylated/")
 unmethylated_data <- lapply(Sys.glob("*.bismark.cov"), read.table)
 
-for(file in methylated_data){
-  colnames(methylated_data[[file]]) <- c("chromosome", "start_position", 
-                                       "end_position", "methylation_percentage", 
-                                       "count_methylated", "count_unmethylated")}
+# adds column names to list of data frame
 
-#causes error because methylated_data is a list not a data frame
+colnames<- c("chromosome", "start_position", 
+             "end_position", "methylation_percentage", 
+             "count_methylated", "count_unmethylated")
 
-#this works:
-
-colnames(methylated_data[[1]]) <- c("chromosome", "start_position", 
-                                 "end_position", "methylation_percentage", 
-                                "count_methylated", "count_unmethylated")
+lapply(methylated_data, setNames, colnames)
+lapply(unmethylated_data, setNames, colnames)
