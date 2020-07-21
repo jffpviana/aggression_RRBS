9489c33f0e3ce28cbfe1b1033fb5047fa73bdcea
zxh755 <zxh755@student.bham.ac.uk>
2020-07-21 16:15:44 +0100

    final script

---
+++ R_scripts/8.limited_coverage_distibution_MCC_boxplot.R
@@ -0,0 +1,42 @@
+# install
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+# BiocManager::install("BiSeq")
+
+library(BiSeq)
+library(stringr)
+library(dplyr)
+
+# cd
+setwd("/rds/projects/v/vianaj-genomics-brain-development/MATRICS/bismark_methylation_extractor/")
+
+# file names
+files <- list.files(pattern="*MCC_r1_trimmed_bismark_bt2.bismark.cov")
+names(files) <- str_match(Sys.glob("*MCC_r1_trimmed_bismark_bt2.bismark.cov"),paste0("BLB","(.*?.....)"))[,1]
+# names(files)[[f]]
+
+rrbs <- readBismark(files, colData = names(files)) #BSraw object
+  
+# DMR
+rrbs.small <- rrbs[1:1000,] 
+# BSraw object but restricted to CpG sites within CpG clusters:
+rrbs.clust.unlim <- clusterSites(object = rrbs.small,
+                                 groups = colData(rrbs)$group,
+                                 perc.samples = 4/5,
+                                 min.sites = 20,
+                                 max.dist = 100)
+
+# smoothing
+ind.cov <- totalReads(rrbs.clust.unlim) > 0
+quant <- quantile(totalReads(rrbs.clust.unlim)[ind.cov], 0.9) # coverage
+rrbs.clust.lim <- limitCov(rrbs.clust.unlim, maxCov = quant) # smooth the methylation values of CpG sites within the clusters
+
+predictedMeth <- predictMeth(object = rrbs.clust.lim) # BSrel object with smoothed relative methylation levels for each CpG site within CpG clusters
+
+# limited coverage distibution plot
+pdf(paste0("/rds/projects/v/vianaj-genomics-brain-development/MATRICS/bismark_methylation_extractor/boxplots/MCC/limited_CpG_coverage_distribution_MCC_boxplot.pdf"))
+covBoxplots(rrbs.clust.lim, col = "cornflowerblue", las = 2)
+dev.off()
+  
+print("done")
