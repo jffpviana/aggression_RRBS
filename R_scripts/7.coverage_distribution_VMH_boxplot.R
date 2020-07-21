d6afa4efd31a58a4da1f07b10f7d127dbf6f1c5b
zxh755 <zxh755@student.bham.ac.uk>
2020-07-21 16:14:18 +0100

    final script

---
+++ R_scripts/7.coverage_distribution_VMH_boxplot.R
@@ -0,0 +1,28 @@
+# install if needed
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+# install.packages("BiocManager")
+
+# BiocManager::install("BiSeq")
+
+library(BiSeq)
+library(stringr)
+library(dplyr)
+
+# cd
+setwd("/rds/projects/v/vianaj-genomics-brain-development/MATRICS/bismark_methylation_extractor/")
+
+# file names
+files <- list.files(pattern="*VMH_r1_trimmed_bismark_bt2.bismark.cov")
+names(files) <- str_match(Sys.glob("*VMH_r1_trimmed_bismark_bt2.bismark.cov"),paste0("BLB","(.*?.....)"))[,1]
+# names(files)[[f]]
+
+for(f in 1:length(files)){
+  rrbs <- readBismark(files[f], colData = names(files)[[f]]) #BSraw object
+  
+  # coverage distibution boxplot 
+  pdf(paste0("/rds/projects/v/vianaj-genomics-brain-development/MATRICS/bismark_methylation_extractor/boxplots/VMH/CpG_coverage_distribution_",names(files)[[f]],"_boxplot.pdf"))
+  covBoxplots(rrbs, col = "cornflowerblue", las = 2)
+  dev.off()
+  
+  print("done")
+}
