8c8b35838f91b7aeda19f94bd6f5e07ffdb7c00d
zxh755 <zxh755@student.bham.ac.uk>
2020-07-24 12:00:51 +0100

    script to run rscripts

---
+++ scripts/10.R_smooth_methylation_levels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --ntasks 5
+#SBATCH --time 60:00:00
+#SBATCH --qos bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+module load bear-apps/2020a
+module load R-bundle-Bioconductor/3.11-foss-2020a-R-4.0.0 # load R
+
+cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/aggression_RRBS/R_scripts/ # cd to R script
+
+# run Rcsript
+# Rscript 9.smooth_methylation_levels_ACC_plot.R
+Rscript 9.smooth_methylation_levels_MCC_plot.R
+Rscript 9.smooth_methylation_levels_VMH_plot.R
