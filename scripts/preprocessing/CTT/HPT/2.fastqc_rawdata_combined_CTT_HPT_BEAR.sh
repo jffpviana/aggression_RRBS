f867e74487abac71a58a1fadc4b010058ad36115
Joana Viana <j.f.viana@exeter.ac.uk>
2020-12-03 14:05:34 +0000

    created scripts to run fastq on CTT HC and HPT

---
+++ scripts/preprocessing/CTT/HPT/2.fastqc_rawdata_combined_CTT_HPT_BEAR.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --ntasks 1
+#SBATCH --time 2:0:0
+#SBATCH --qos bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+
+module load FastQC/0.11.9-Java-11 #load the module fastqc
+
+cd /rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTT/HPT/combined #change directory
+
+fastqc *.fastq -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTT/HPT/combined/fastqc # run fastqc for all samples and output in the directory of the respective cohort
+
+fastqc *.fq -o /rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTT/HPT/combined/fastqc # run fastqc for all samples and output in the directory of the respective cohort
