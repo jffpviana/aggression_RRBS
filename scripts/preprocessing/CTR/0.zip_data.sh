7672f274f004274af9d5fa9093e76f7c55614b3a
Joana Viana <j.f.viana@exeter.ac.uk>
2020-12-04 10:34:44 +0000

    zip data

--- scripts/preprocessing/CTR/0.zip_data.sh
+++ scripts/preprocessing/CTR/0.zip_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --ntasks 8
-#SBATCH --time 5:0:0
+#SBATCH --time 10:0:0
 #SBATCH --qos bbdefault
 #SBATCH --mail-type ALL
 
@@ -9,4 +9,4 @@ set -e
 module purge; module load bluebear
 
 
-gunzip -d /rds/projects/v/vianaj-genomics-brain-development/MATRICS/CTR/HPT/trim_galore_output/*.fq.gz # Unzip the .fq files in all sub folders
+gzip /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/trim_galore_output/*.fq # Unzip the .fq files in all sub folders
