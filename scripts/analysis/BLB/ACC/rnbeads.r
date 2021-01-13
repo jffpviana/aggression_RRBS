305350a3f6df10fb20afac6e34aae119cb873fb4
Joana Viana <j.f.viana@exeter.ac.uk>
2021-01-13 16:49:11 +0000

    development rnbeads script

--- scripts/analysis/BLB/ACC/rnbeads.r
+++ scripts/analysis/BLB/ACC/rnbeads.r
@@ -1,10 +1,20 @@
 library(RnBeads)
+library("RnBeads.mm10")
 
 
-  input_dir  <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/methylation/"
-  output_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/analysis/"
+cohort <- "BLB"
+region <- "ACC"
+input_dir  <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/methylation"
+output_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/analysis/"
+reports_dir <- "/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/analysis/reports"
+sample_annotation <-"/rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/BLBACC_pheno.csv"
+genome <- "mm10"
+analysis_name <- print0(cohor, "_", region)
 
+rnb.options(import.bed.style = "bismarkCov", assembly = genome, analysis.name = analysis_name) # RnBeads options, such as type of data file and genome assembly
 
-result <- rnb.run.import(data.source=data.source,
-+ data.type="infinium.idat.dir", dir.reports=report.dir)
-> rnb.set <- result$rnb.set
+data_source <- c(input_dir, sample_annotation) # source of data and phenotype (sample annotation file)
+
+data_meth <- rnb.run.import(data.source=data_source, data.type="bs.bed.dir",  dir.reports=reports_dir)
+
+rnb_set <- data_meth$rnb.set
--- scripts/analysis/BLB/ACC/rnbeads.sh
+++ scripts/analysis/BLB/ACC/rnbeads.sh
@@ -1,4 +1,8 @@
 module load slurm-interactive #load module
-fisbatch_screen --ntasks 1 --time 8:0:0
+fisbatch_screen --ntasks 4 --time 8:0:0
 
 module load R/3.6.2-fosscuda-2019b
+
+gunzip -d /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/methylation/*.cov.gz
+
+gunzip -d /rds/projects/v/vianaj-genomics-brain-development/MATRICS/BLB/ACC/methylation/*.bedGraph.gz
